bf774fe5629e10d19a099e339bcf44a87063b5ff
JorgeSerranoP <100405987@alumnos.uc3m.es>
2021-03-16 16:47:46 +0100

    Todas las operaciones y formato salida correcto

--- src/lex/manual/Driver.java
+++ src/lex/manual/Driver.java
@@ -13,21 +13,8 @@ import java.util.ArrayList;
 class Driver {
 	public static void main(String args[]) throws IOException {
 		// Entrada de datos: fichero especificado en los argumentos
-		FileInputStream dataStreamBuffer = new FileInputStream(args[0]);
-		FileInputStream dataStreamScanner = new FileInputStream(args[0]);
-
 		System.out.println("Leyendo entrada de fichero... ");
-		InputStreamReader isr = new InputStreamReader(dataStreamBuffer, "utf8");
-		BufferedReader br = new BufferedReader(isr);
-
-		String linea;
-		int lineas = 0;
-		int caracteres = 0;
-		while ((linea = br.readLine()) != null) {
-			lineas++;
-			caracteres += linea.length();
-		}
-
+		FileInputStream dataStreamScanner = new FileInputStream(args[0]);
 		// Creamos el objeto scanner y los arraylist para cada tipo de simbolo
 		Lexer scanner = new Lexer(dataStreamScanner);
 		ArrayList<Symbol> simbolos = new ArrayList<Symbol>();
@@ -72,11 +59,11 @@ class Driver {
 				if (simbolos.get(i).type() == 3 || simbolos.get(i).type() == 5 || simbolos.get(i).type() == 6) {
 					operadores.add(simbolos.get(i));
 				}
-				/*if (i != 0 && i != simbolos.size() - 1 && simbolos.get(i - 1).type() == 9
+				if (i != 0 && i != simbolos.size() - 1 && simbolos.get(i - 1).type() == 9
 						&& simbolos.get(i + 1).type() == 9 && simbolos.get(i).type() == 4) {
-					operadores.add((Symbol)"+");
+					operadores.add((new Symbol(3, "+")));
 					contadorMenos1++;
-				}*/
+				}
 				if (simbolos.get(i).type() == 4) {
 					contadorMenos++;
 				}
@@ -86,27 +73,31 @@ class Driver {
 							for (int k = 0; k < contadorMenos; k++) {
 								result = numeros.get(j) + numeros.get(j + 1);
 								numeros.set(j + 1, result);
+								System.out.println("Operacion: " + numeros.get(j) + " - " + numeros.get(j + 1) + " = " + result);
 								j += 1;
 							}
 						}
-						for (int n = 0; n < operadores.size() /*+ contadorMenos1*/; n++) {
+						for (int n = 0; n < operadores.size() /* + contadorMenos1 */; n++) {
 							if (operadores.get(n).type() == 3) {
 								result = numeros.get(j) + numeros.get(j + 1);
 								numeros.set(j + 1, result);
+								System.out.println("Operacion: " + numeros.get(j) + " + " + numeros.get(j + 1) + " = " + result);
 								j += 1;
 							} else if (operadores.get(n).type() == 5) {
 								result = numeros.get(j) * numeros.get(j + 1);
+								System.out.println("Operacion: " + numeros.get(j) + " * " + numeros.get(j + 1) + " = " + result);
 								numeros.set(j + 1, result);
 								j += 1;
 							} else if (operadores.get(n).type() == 6) {
 								result = numeros.get(j) / numeros.get(j + 1);
+								System.out.println("Operacion: " + numeros.get(j) + " / " + numeros.get(j + 1) + " = " + result);
 								numeros.set(j + 1, result);
 								j += 1;
 							}
 						}
 
 					}
-					System.out.println(result);
+					System.out.println("Resultado: " + result);
 					numeros.clear();
 					operadores.clear();
 					result = 0;
